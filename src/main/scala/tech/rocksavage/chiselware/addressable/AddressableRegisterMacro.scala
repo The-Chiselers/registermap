a427fa5d1bb5529ce1eb5b499da5fe4abfa58f49
Nick Allison <nickrallison1@gmail.com>
2025-01-06 11:52:50 -0700

    improving apb macro, fixes seq issue

--- src/main/scala/tech/rocksavage/chiselware/addressable/AddressableRegisterMacro.scala
+++ src/main/scala/tech/rocksavage/chiselware/addressable/AddressableRegisterMacro.scala
@@ -10,41 +10,47 @@ object AddressableRegisterMacro {
     import c.universe._
 
     // Extract the annotated variable and its properties
-    val (registerName, registerType, registerWidth) = annottees.map(_.tree) match {
+    val (registerName, registerType, registerWidth, writeCallback) = annottees.map(_.tree) match {
       case List(q"val $name = $init") =>
         c.abort(c.enclosingPosition, "Must specify type for register")
       case List(q"val $name: $tpe = RegInit($init)") =>
-        val width = tpe match {
-          case tq"UInt" => {
-            init match {
-              case q"$default.U($width.W)" => c.eval(c.Expr[Int](width))
+        val (width, writeCallback) = tpe match {
+          case tq"UInt" =>
+            // Extract the width from the initialization expression
+            val width = init match {
+              case q"$default.U($width.W)" => width
+              case _ => c.abort(c.enclosingPosition, s"Unsupported initialization for UInt register $name")
             }
-          }
-          case tq"Bool" => 1
+            (width, q"(value: UInt) => $name := value")
+          case tq"Bool" =>
+            (q"1", q"(value: UInt) => $name := value(0)")
           case _ => c.abort(c.enclosingPosition, s"Unsupported type for register $name: $tpe")
         }
-        (name, tpe, width)
+        (name, tpe, width, writeCallback)
       case _ => c.abort(c.enclosingPosition, "Invalid annotation target")
     }
 
-    // Generate the read and write functions
+    // Generate the read function
     val readFunction = q"""
       () => $registerName
     """
-    val writeFunction = q"""
-      (value: $registerType) => $registerName := value
+
+    // Generate the code to define the register and add it to the RegisterMap
+    val result = q"""
+      val $registerName = {
+        val $registerName = RegInit(${annottees.head.tree.asInstanceOf[ValDef].rhs.asInstanceOf[Apply].args.head})
+        registerMap.addRegister(
+          ${registerName.toString},
+          $registerWidth,
+          $readFunction,
+          $writeCallback
+        )
+        $registerName
+      }
     """
 
-    // Generate the code to add the register to the RegisterMap
-    val result = q"""
-      val $registerName = RegInit(${annottees.head.tree.asInstanceOf[ValDef].rhs})
-      registerMap.addRegister(
-        ${registerName.toString},
-        $registerWidth,
-        $readFunction,
-        $writeFunction
-      )
-    """
+    // Print the generated code for debugging
+    c.info(c.enclosingPosition, s"Generated code: ${showCode(result)}", force = true)
 
     c.Expr[Any](result)
   }
